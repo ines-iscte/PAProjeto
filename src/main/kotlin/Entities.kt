b84e61d627417034e48f08a76911e6a4a54779e6
ines-iscte <icaoo1@iscte-iul.pt>
2024-04-08 21:26:54 +0100

    Ponto 4 feito Confirmar tag de fecho em componentes

--- src/main/kotlin/Entities.kt
+++ src/main/kotlin/Entities.kt
@@ -1,11 +1,13 @@
-data class Attribute(
+import java.io.File
+
+class Attribute(
     var name: String,
     var value: String
 )
 
 class Entity(
     val name: String,
-    val text: String = "",
+    var text: String = "",
     val attributes: MutableList <Attribute> = mutableListOf(),
     val parent: Entity? = null,
     val children: MutableList<Entity> = mutableListOf()
@@ -13,7 +15,7 @@ class Entity(
     ){
     init{
         if (parent != null) {
-            parent?.children.add(this)
+            parent.children.add(this)
         }
 
     }
@@ -25,10 +27,11 @@ class Entity(
     }
 
     fun removeAttribute(attribute: Attribute){
-        attributes.forEach(){
-            if(it == attribute)
-                attributes.remove(it)
-        }
+//        attributes.forEach(){
+//            if(it == attribute)
+//                attributes.remove(it)
+//        }
+        attributes.removeIf { it == attribute }
     }
 
     fun changeAttribute(attribute: Attribute, new_name: String? = null, new_value: String? = null) {
@@ -44,6 +47,23 @@ class Entity(
         }
     }
 
+    fun get_attribute(): MutableList<Attribute>{
+        return this.attributes
+    }
+
+    // Texto
+    fun get_entity_text(): String{
+        return this.text
+    }
+
+    fun change_entity_text(new_text: String){
+        text=new_text
+    }
+
+    fun remove_entity_text(){
+        text=""
+    }
+
     // Entidade mãe e entidades aninhadas
 
     fun get_Parent(): Entity?{
@@ -51,12 +71,12 @@ class Entity(
     }
 
     fun get_Children() : List<Entity> {
-        var list: MutableList<Entity> = mutableListOf()
+        val list: MutableList<Entity> = mutableListOf()
         this.children.forEach {
-            if (it is Entity) {
-                list.add(it)
-                list.addAll(it.get_Children())
-            }
+            //if (it is Entity) {
+            list.add(it)
+            list.addAll(it.get_Children())
+            //}
         }
         return list
     }
@@ -78,19 +98,90 @@ class Entity(
 class Document(
     val name: String,
     val entities: MutableList <Entity> = mutableListOf()
-){
+) {
 
-    fun addEntity(entity: Entity){
+    fun addEntity(entity: Entity) {
         entities.add(entity)
     }
 
-    fun removeEntity(entity: Entity){
-        entities.forEach(){
-            if(it == entity)
-                entities.remove(it)
-        }
+    fun removeEntity(entity: Entity) {
+//        entities.forEach() {
+//            if (it == entity)
+//                entities.remove(it)
+//        }
+        entities.removeIf { it == entity }
     }
+
+    fun prettyPrint(entity: Entity, indent: String = ""): String {
+        val stringBuilder = StringBuilder()
+
+        stringBuilder.append("$indent<${entity.name}")
+        if (entity.attributes.isNotEmpty()) {
+            entity.attributes.forEach { attribute ->
+                stringBuilder.append(" ${attribute.name}=\"${attribute.value}\"")
+            }
+        }
+
+        //stringBuilder.append(">")
+        // Tirar esta identação se for para ter tag no fim
+        if (entity.children.isEmpty() && entity.text.isEmpty()) {
+            stringBuilder.append("/>")
+        } else {
+            stringBuilder.append(">")
+
+            if (entity.text.isNotEmpty())
+                stringBuilder.append("${entity.text}")
+
+            if (entity.children.isNotEmpty()) {
+                stringBuilder.appendLine()
+                entity.children.forEach { child ->
+                    stringBuilder.append(prettyPrint(child, "$indent    "))
+                    stringBuilder.appendLine()
+                }
+                stringBuilder.append("$indent")
+            }
+
+            // WTF porque é que "componente" não tem tag de fecho
+            // Que estupidez i swear
+            if (entity.children.isNotEmpty() || entity.text.isNotEmpty()) {
+                stringBuilder.append("</${entity.name}>")
+            }
+        }
+        return stringBuilder.toString()
+    }
+
+    fun prettyPrintToFile(entity: Entity, indent: String = "", outputFile: File) {
+        val xmlString = prettyPrint(entity, indent)
+        outputFile.writeText(xmlString)
+    }
+
 }
+
+//    fun prettyPrintDocument(): String {
+
+//        var file: String
+//
+//        file = """<?xml version="1.0" encoding="UTF-8"?>"""
+//        entities.forEach {
+//            //if (it.children.isEmpty()) {
+//            file.plus("<" + it.name + ">")
+//            if (it.text != "")
+//                file.plus(it.text)
+//            if (!it.attributes.isEmpty())
+//                it.attributes.forEach {
+//                    file.plus(it.name + "=" + it.value)
+//                }
+//
+//            file.plus("</" + it.name + ">")
+//            //}
+//            file.plus("<" + it.name + ">")
+//
+//        }
+//    return file
+//    }
+//
+//
+//}
 
 
 //data class InnerEntity(
--- src/test/kotlin/Tests.kt
+++ src/test/kotlin/Tests.kt
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test
+import java.io.File
 import kotlin.test.assertEquals
 
 class Tests {
@@ -9,6 +10,38 @@ class Tests {
     val papel = Attribute("Material", "Papel")
     val garrafa =  Entity(name="garrafa", attributes= mutableListOf(plastico))
     val doc = Document(name="doc")
+
+    val doc_plano = Document(name="doc_plano")
+
+    // A usar no teste de Pretty Print (Ponto 4.)
+    val plano = Entity(name="plano")
+    val curso1 = Entity(name="curso", text="Mestrado em Engenharia Informática", parent = plano)
+    val codigo1 = Attribute(name="codigo", value="M4310")
+    val fuc1 = Entity(name="fuc", attributes= mutableListOf(codigo1), parent = plano)
+    val nome1 = Entity(name="nome", text="Programação Avançada", parent=fuc1)
+    val ects1 = Entity(name="ects", text="6.0", parent=fuc1)
+    val avaliacao1 = Entity(name="avaliacao", parent=fuc1)
+    val componente_nome1 = Attribute(name="nome", value="Quizzes")
+    val componente_nome2 = Attribute(name="peso", value="20%")
+    val componente1 = Entity(name="componente", attributes= mutableListOf(componente_nome1, componente_nome2), parent=avaliacao1)
+    val componente_nome3 = Attribute(name="nome", value="Projeto")
+    val componente_nome4 = Attribute(name="peso", value="80%")
+    val componente2 = Entity(name="componente", attributes= mutableListOf(componente_nome3, componente_nome4), parent=avaliacao1)
+
+    val codigo2 = Attribute(name="codigo", value="03782")
+    val fuc2 = Entity(name="fuc", attributes= mutableListOf(codigo2), parent = plano)
+    val nome2 = Entity(name="nome", text="Dissertação", parent=fuc2)
+    val ects2 = Entity(name="ects", text="42.0", parent=fuc2)
+    val avaliacao2 = Entity(name="avaliacao", parent=fuc2)
+    val componente_nome5 = Attribute(name="nome", value="Dissertação")
+    val componente_nome6 = Attribute(name="peso", value="60%")
+    val componente3 = Entity(name="componente", attributes= mutableListOf(componente_nome5, componente_nome6), parent=avaliacao2)
+    val componente_nome7 = Attribute(name="nome", value="Apresentação")
+    val componente_nome8 = Attribute(name="peso", value="20%")
+    val componente4 = Entity(name="componente", attributes= mutableListOf(componente_nome7, componente_nome8), parent=avaliacao2)
+    val componente_nome9 = Attribute(name="nome", value="Discussão")
+    val componente_nome10 = Attribute(name="peso", value="20%")
+    val componente5 = Entity(name="componente", attributes= mutableListOf(componente_nome9, componente_nome10), parent=avaliacao2)
 
     // Testes a Atributos
     @Test
@@ -116,4 +149,141 @@ class Tests {
         doc.removeEntity(modelo)
         assertEquals(listOf(peso, garrafa), doc.entities)
     }
+
+    // Ponto 4.
+    @Test
+    fun test_pretty_print(){
+//        doc.addEntity(peso)
+//        val modelo = Entity(name="modelo", text="Automatico", parent=peso)
+//        val garrafa =  Entity(name="garrafa", attributes= mutableListOf(plastico), parent=peso)
+//        doc.addEntity(modelo)
+//        doc.addEntity(garrafa)
+
+        doc_plano.addEntity(plano)
+        doc_plano.addEntity(curso1)
+        doc_plano.addEntity(fuc1)
+        doc_plano.addEntity(nome1)
+        doc_plano.addEntity(ects1)
+        doc_plano.addEntity(avaliacao1)
+        doc_plano.addEntity(componente1)
+        doc_plano.addEntity(componente2)
+        doc_plano.addEntity(fuc2)
+        doc_plano.addEntity(nome2)
+        doc_plano.addEntity(ects2)
+        doc_plano.addEntity(avaliacao2)
+        doc_plano.addEntity(componente3)
+        doc_plano.addEntity(componente4)
+        doc_plano.addEntity(componente5)
+
+        val expected_output = """
+        <plano>
+            <curso>Mestrado em Engenharia Informática</curso>
+            <fuc codigo="M4310">
+                <nome>Programação Avançada</nome>
+                <ects>6.0</ects>
+                <avaliacao>
+                    <componente nome="Quizzes" peso="20%"/>
+                    <componente nome="Projeto" peso="80%"/>
+                </avaliacao>
+            </fuc>
+            <fuc codigo="03782">
+                <nome>Dissertação</nome>
+                <ects>42.0</ects>
+                <avaliacao>
+                    <componente nome="Dissertação" peso="60%"/>
+                    <componente nome="Apresentação" peso="20%"/>
+                    <componente nome="Discussão" peso="20%"/>
+                </avaliacao>
+            </fuc>
+        </plano>
+    """.trimIndent()
+
+        assertEquals(expected_output, doc_plano.prettyPrint(doc_plano.entities[0]))
+    }
+
+    // Ponto 4.
+    @Test
+    fun test_pretty_print_to_file(){
+        doc_plano.addEntity(plano)
+        doc_plano.addEntity(curso1)
+        doc_plano.addEntity(fuc1)
+        doc_plano.addEntity(nome1)
+        doc_plano.addEntity(ects1)
+        doc_plano.addEntity(avaliacao1)
+        doc_plano.addEntity(componente1)
+        doc_plano.addEntity(componente2)
+        doc_plano.addEntity(fuc2)
+        doc_plano.addEntity(nome2)
+        doc_plano.addEntity(ects2)
+        doc_plano.addEntity(avaliacao2)
+        doc_plano.addEntity(componente3)
+        doc_plano.addEntity(componente4)
+        doc_plano.addEntity(componente5)
+
+        val expected_output = """
+        <plano>
+            <curso>Mestrado em Engenharia Informática</curso>
+            <fuc codigo="M4310">
+                <nome>Programação Avançada</nome>
+                <ects>6.0</ects>
+                <avaliacao>
+                    <componente nome="Quizzes" peso="20%"/>
+                    <componente nome="Projeto" peso="80%"/>
+                </avaliacao>
+            </fuc>
+            <fuc codigo="03782">
+                <nome>Dissertação</nome>
+                <ects>42.0</ects>
+                <avaliacao>
+                    <componente nome="Dissertação" peso="60%"/>
+                    <componente nome="Apresentação" peso="20%"/>
+                    <componente nome="Discussão" peso="20%"/>
+                </avaliacao>
+            </fuc>
+        </plano>
+    """.trimIndent()
+
+        val filePath = "output.xml"
+        val testFile = File(filePath)
+
+        // Chame a função para escrever a representação formatada no arquivo
+        doc_plano.prettyPrintToFile(doc_plano.entities[0], outputFile = testFile)
+
+        // Verifique se o arquivo foi criado
+        assert(testFile.exists())
+
+        // Verifique se o conteúdo do arquivo é o mesmo que a representação formatada da entidade
+        val actualXml = testFile.readText()
+        assertEquals(expected_output, actualXml)
+
+        // Limpeza: Exclua o arquivo após o teste
+        testFile.delete()
+    }
+        //assertEquals(expected_output, doc_plano.prettyPrint(doc_plano.entities[0]))
+
+    // Testes auxiliares
+    @Test
+    fun test_get_attributes(){
+        assertEquals(mutableListOf(plastico), garrafa.get_attribute())
+    }
+
+    @Test
+    fun test_get_entity_text(){
+        assertEquals("Automático", modelo.get_entity_text())
+    }
+
+    @Test
+    fun test_change_entity_text(){
+        modelo.change_entity_text("Não automático")
+        assertEquals("Não automático", modelo.get_entity_text())
+    }
+
+    @Test
+    fun test_remove_entity_text(){
+        modelo.remove_entity_text()
+        assertEquals("", modelo.get_entity_text())
+    }
+
+
+
 }
